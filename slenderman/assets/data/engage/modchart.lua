d1973efd0afcc1ef3527980c487e59c1d930f84d
ggl22 <88840086+ggl22@users.noreply.github.com>
2021-08-17 07:40:55 +0300

    Add files via upload

---
+++ slenderman/assets/data/engage/modchart.lua
@@ -0,0 +1,19 @@
+function start (song)
+		print("Succesful Engage")
+end
+
+function update (elapsed)
+		
+end
+
+function beatHit (beat)
+		
+end
+
+function stepHit (step)
+		
+end
+
+function keyPressed (key)
+		
+end
